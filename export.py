5aac72d2f3c51fbc192e83df4bba664138aa27b6
udav516 <eldar.udav516@yandex.ru>
2022-09-28 15:38:17 +0300

    Task Database system export

--- export.py
+++ export.py
@@ -1,5 +1,10 @@
+import log
+
 def data_add():
     f = open('data.txt', 'a', encoding="utf-8")
     user_list = input('Введите данные через пробел: ')
     f.writelines(f'\n{user_list}')
     f.close()
+    log.log_add(user_list)
+
+
