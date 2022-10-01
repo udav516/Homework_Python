bfc605f67f7add0af2178a7d2dcde8cbff302362
udav516 <eldar.udav516@yandex.ru>
2022-10-01 20:36:07 +0300

    Task 46

---
+++ Task46.py
@@ -0,0 +1,12 @@
+# Дана строка в виде случайной последовательности чисел от 0 до 9.
+# Требуется создать словарь, который в качестве ключей будет принимать данные числа (т. е. ключи будут типом int),
+# а в качестве значений – количество этих чисел в имеющейся последовательности.
+# Для построения словаря создайте функцию count_it(sequence), принимающую строку из цифр.
+# Функция должна возвратить словарь из 3-х самых часто встречаемых чисел.
+
+def count_it(sequence):
+    num_frequency = {int(item): sequence.count(item) for item in sequence}
+    sorted_num_frequency = sorted(num_frequency.items(), key=lambda element: element[1])
+    return dict(sorted_num_frequency[-3:])
+
+print(count_it('771556245'))
